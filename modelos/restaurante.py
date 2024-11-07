dcde51520fcd10048870467c154c07bb38a2ea38
s0d4z3r0 <av.falcao@hotmail.com>
2024-11-07 18:38:26 -0300

    Diferenciando métodos de classe e de objeto

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -9,9 +9,10 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self.categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do Restaurante'.ljust(28)} | {'Categoria'.ljust(25)} | {'Status'}')
-        for indice, restaurante in enumerate(Restaurante.restaurantes):
+        for indice, restaurante in enumerate(cls.restaurantes):
             id = indice + 1
             print(f'{id}- {restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
             
@@ -19,9 +20,12 @@ class Restaurante:
     def ativo(self):
         return 'Ativo ✔️' if self._ativo else 'Inativo ❌'
     
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
         
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca._nome = 'Praça 2.0'
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza Express', 'Italiano')
 
 Restaurante.listar_restaurantes()
