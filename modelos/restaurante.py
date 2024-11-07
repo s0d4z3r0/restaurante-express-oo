758196846c7d7fe708a6d043fc497d06712689fc
s0d4z3r0 <av.falcao@hotmail.com>
2024-11-07 15:19:21 -0300

    Utilizando método __str__ para retornar objeto formatado.

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,11 +3,14 @@ class Restaurante:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
     
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
