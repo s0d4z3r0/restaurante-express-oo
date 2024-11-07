bcd4a39e7f70e61b1fd45747aee5ffd5f38620ac
s0d4z3r0 <av.falcao@hotmail.com>
2024-11-07 18:33:25 -0300

    Conhecendo os atributos privados.

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,25 +1,27 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do Restaurante'.ljust(28)} | {'Categoria'.ljust(25)} | {'Status'}')
         for indice, restaurante in enumerate(Restaurante.restaurantes):
             id = indice + 1
-            print(f'{id}- {restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{id}- {restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
             
     @property
     def ativo(self):
         return 'Ativo ✔️' if self._ativo else 'Inativo ❌'
     
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
+        
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca._nome = 'Praça 2.0'
+restaurante_pizza = Restaurante('pizza Express', 'Italiano')
 
 Restaurante.listar_restaurantes()
