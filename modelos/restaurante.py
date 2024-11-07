f8c35aacef5957e159b34b196c0f2176bf3144b6
s0d4z3r0 <av.falcao@hotmail.com>
2024-11-07 18:25:24 -0300

    Trabalhando com @property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,15 +3,21 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+        print(f'{'Nome do Restaurante'.ljust(28)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for indice, restaurante in enumerate(Restaurante.restaurantes):
+            id = indice + 1
+            print(f'{id}- {restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            
+    @property
+    def ativo(self):
+        return 'Ativo ✔️' if self._ativo else 'Inativo ❌'
     
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
