b57f358ec0ef47794175eab6dbb081cb3590272c
s0d4z3r0 <av.falcao@hotmail.com>
2024-11-07 14:38:46 -0300

    Conceituando Atributos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,8 +4,10 @@ class Restaurante:
     ativo = False
     
 restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+print(vars(restaurante_praca))
